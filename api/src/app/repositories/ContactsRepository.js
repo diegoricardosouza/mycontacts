2ff07926d70e2d9616597d7780df8043c20cdad3
Diego Ricardo <diegoricardoweb@gmail.com>
2022-01-15 20:32:44 -0300

    Refatorado metodos Find

--- api/src/app/repositories/ContactsRepository.js
+++ api/src/app/repositories/ContactsRepository.js
@@ -20,23 +20,19 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  findAll() {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+    return rows;
   }
 
-  findById(id) {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    return row;
   }
 
   delete(id) {
