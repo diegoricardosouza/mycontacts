7b41c3c145d04483e82a79858a0520e54201c315
Diego Ricardo <diegoricardoweb@gmail.com>
2022-01-15 21:11:54 -0300

    Refatorado metodo Delete

--- api/src/app/repositories/ContactsRepository.js
+++ api/src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Diego',
-    email: 'diego@email.com',
-    phone: '123123456',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jose',
-    email: 'jose@email.com',
-    phone: '1231asdfasfas23456',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -60,12 +41,12 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE FROM contacts
+      WHERE id = $1
+    `, [id]);
+    return deleteOp;
   }
 }
 
