d7bbf7b2e99167a5531ad4c20dff16e7bc7693af
Diego Ricardo <diegoricardoweb@gmail.com>
2022-02-12 20:02:18 -0300

    Criado lista de contatos

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -1,5 +1,9 @@
 import * as S from './styles';
 
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function ContactsList() {
   return (
     <S.Container>
@@ -7,6 +11,76 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </S.Header>
+
+      <S.ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <S.Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diego Ricardo</strong>
+              <small>instagram</small>
+            </div>
+            <span>diego@gmail.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </S.Card>
+        <S.Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diego Ricardo</strong>
+              <small>instagram</small>
+            </div>
+            <span>diego@gmail.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </S.Card>
+        <S.Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diego Ricardo</strong>
+              <small>instagram</small>
+            </div>
+            <span>diego@gmail.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </S.Card>
+      </S.ListContainer>
     </S.Container>
   );
 }
--- fe/src/components/ContactsList/styles.js
+++ fe/src/components/ContactsList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   justify-content: space-between;
 
   strong {
-    color: #222;
     font-size: 2.4rem;
   }
 
@@ -26,6 +25,94 @@ export const Header = styled.header`
     &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #FFF;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 2.4rem;
+
+  header {
+    margin-bottom: 0.8rem;
+
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        font-size: 1.6rem;
+        color: ${({ theme }) => theme.colors.primary.main};
+        margin-right: 0.8rem;
+        font-weight: 700;
+      }
+
+      img {
+        max-height: 1.55rem;
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #FFF;
+  box-shadow: 0 0.4rem 1rem rgba(0, 0, 0, 0.04);
+  border-radius: 0.4rem;
+  padding: 1.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 1.6rem;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: 700;
+        text-transform: uppercase;
+        font-size: 1.2rem;
+        padding: 0.4rem;
+        border-radius: 0.4rem;
+        margin-left: 0.8rem;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 1.4rem;
+      color: ${({ theme }) => theme.colors.gray[200]};
+      margin-top: 0.4rem;
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 0.8rem;
+
+      img {
+        height: 2.2rem;
+      }
+    }
+
+    a {
+      display: flex;
+
+      img {
+        height: 2.01rem;
+      }
     }
   }
 `;
