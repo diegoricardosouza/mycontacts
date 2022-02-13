6c3d7c15430d289b30176c8053f449a0e6508e31
Diego Ricardo <diegoricardoweb@gmail.com>
2022-02-13 00:11:28 -0300

    Criado componente Button

---
+++ fe/src/components/Button.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  width: 100%;
+  border: none;
+  background: ${({ theme }) => theme.colors.primary.main};
+  box-shadow: 0 0.4rem 1rem rgba(0, 0, 0, 0.04);
+  border-radius: 0.4rem;
+  height: 5.2rem;
+  font-size: 1.6rem;
+  outline: none;
+  padding: 0 1.6rem;
+  color: #FFF;
+  font-weight: 700;
+  transition: background 0.2s ease-in;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primary.light};
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.primary.dark};
+  }
+
+  &[disabled] {
+    background: #CCC;
+    cursor: not-allowed;
+  }
+`;
