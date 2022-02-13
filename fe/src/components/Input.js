47dbb5aa5b379f5c8270e2fdcbd7899deb63c0a0
Diego Ricardo <diegoricardoweb@gmail.com>
2022-02-13 00:00:05 -0300

    Criado componentes Input e Select

---
+++ fe/src/components/Input.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  border: 0.2rem solid #FFF;
+  background: #FFF;
+  box-shadow: 0 0.4rem 1rem rgba(0, 0, 0, 0.04);
+  border-radius: 0.4rem;
+  height: 5.2rem;
+  font-size: 1.6rem;
+  outline: none;
+  padding: 0 1.6rem;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray[200]};
+  }
+`;
