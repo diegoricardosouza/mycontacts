f49904cc31b53829ceb5eb5b527d764c7c4c9ac8
Diego Ricardo <diegoricardoweb@gmail.com>
2022-03-07 23:26:38 -0300

    Adicionado ordenação da listagem

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -8,9 +8,10 @@ import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
 
   useEffect(() => {
-    fetch('http://localhost:3001/contacts')
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
         const json = await response.json();
         setContacts(json);
@@ -18,7 +19,11 @@ export default function Home() {
       .catch((error) => {
         console.log('erro', error);
       });
-  }, []);
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }
 
   return (
     <S.Container>
@@ -36,37 +41,35 @@ export default function Home() {
         </Link>
       </S.Header>
 
-      <S.ListContainer>
-        <header>
-          <button type="button">
-            <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
-          </button>
-        </header>
+      <S.ListHeader orderBy={orderBy}>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span>Nome</span>
+          <img src={arrow} alt="Arrow" />
+        </button>
+      </S.ListHeader>
 
-        {contacts.map((contact) => (
-          <S.Card key={contact.id}>
-            <div className="info">
-              <div className="contact-name">
-                <strong>{contact.name}</strong>
-                {contact.category_name && <small>{contact.category_name}</small>}
-              </div>
-              <span>{contact.email}</span>
-              <span>{contact.phone}</span>
+      {contacts.map((contact) => (
+        <S.Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              {contact.category_name && <small>{contact.category_name}</small>}
             </div>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
 
-            <div className="actions">
-              <Link to={`/edit/${contact.id}`}>
-                <img src={edit} alt="Edit" />
-              </Link>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={edit} alt="Edit" />
+            </Link>
 
-              <button type="button">
-                <img src={trash} alt="Delete" />
-              </button>
-            </div>
-          </S.Card>
-        ))}
-      </S.ListContainer>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </S.Card>
+      ))}
     </S.Container>
   );
 }
--- fe/src/pages/Home/styles.js
+++ fe/src/pages/Home/styles.js
@@ -50,28 +50,27 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
+export const ListHeader = styled.header`
   margin-top: 2.4rem;
+  margin-bottom: 0.8rem;
 
-  header {
-    margin-bottom: 0.8rem;
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
 
-    button {
-      background: transparent;
-      border: none;
-      display: flex;
-      align-items: center;
+    span {
+      font-size: 1.6rem;
+      color: ${({ theme }) => theme.colors.primary.main};
+      margin-right: 0.8rem;
+      font-weight: 700;
+    }
 
-      span {
-        font-size: 1.6rem;
-        color: ${({ theme }) => theme.colors.primary.main};
-        margin-right: 0.8rem;
-        font-weight: 700;
-      }
-
-      img {
-        width: 1.5rem;
-      }
+    img {
+      width: 1.5rem;
+      transform: ${({ orderBy }) => (orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)')};
+      transition: transform 0.2s ease-in;
     }
   }
 `;
