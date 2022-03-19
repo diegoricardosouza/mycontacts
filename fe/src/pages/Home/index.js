4a81554d4138c0e07bd0aed0270155d9affc2c1e
Diego Ricardo <diegoricardoweb@gmail.com>
2022-03-18 23:38:58 -0300

    Adicionado mensagem de nenhum contato encontrado

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -11,6 +11,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import ContactsService from '../../services/ContactsService';
@@ -108,6 +109,16 @@ export default function Home() {
                 para cadastrar o seu primeiro!
               </p>
             </S.EmptyListContainer>
+          )}
+
+          {(contacts.length > 0 && filteredContacts.length < 1) && (
+            <S.SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier Question" />
+
+              <span>
+                Nenhum resultado foi encontrado para <strong>”{searchTerm}”</strong>.
+              </span>
+            </S.SearchNotFoundContainer>
           )}
 
           {filteredContacts.length > 0 && (
--- fe/src/pages/Home/styles.js
+++ fe/src/pages/Home/styles.js
@@ -172,3 +172,15 @@ export const EmptyListContainer = styled.div`
     }
   }
 `;
+
+export const SearchNotFoundContainer = styled.div`
+  margin-top: 1.6rem;
+  display: flex;
+  align-items: flex-start;
+
+  span {
+    color: ${({ theme }) => theme.colors.gray[200]};
+    margin-left: 2.4rem;
+    word-break: break-word;
+  }
+`;
